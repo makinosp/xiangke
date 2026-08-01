0a294a0d54bdb0f422aceca7b0ce50d11b34eeb3
makinosp <makinosp@icloud.com>
2026-08-01 13:55:02 +0900

    test(gdscript): add battle flow service tests for front-line combat
    - Cover front participant fielding at battle start
    - Cover player/enemy switches and stat preservation
    - Cover front-targeted attacks and automatic bench replacement

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -10,6 +10,7 @@ const TEST_SCRIPTS: Array[String] = [
 	"res://tests/unit/test_ui_focus_manager.gd",
 	"res://tests/unit/test_type_enums.gd",
 	"res://tests/unit/test_type_chart.gd",
+	"res://tests/unit/test_battle_flow_service.gd",
 ]
 
 ## Total counters
